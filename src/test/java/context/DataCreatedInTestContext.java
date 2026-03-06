78af6b70ff7bcaff478914708d67ec39777d40e7
Emerson Lima <emersondll@outlook.com>
2026-03-06 11:03:58 -0300

    Commit automático do arquivo src/test/java/context/DataCreatedInTestContext.java

---
+++ src/test/java/context/DataCreatedInTestContext.java
@@ -0,0 +1,110 @@
+package context;
+
+public class DataCreatedInTestContext {
+
+	private boolean dataCreatedInPriceRelayV2;
+	private boolean dataCreatedInPriceRelayV3;
+	private boolean dataCreatedInDealRelayV2;
+	private boolean dataCreatedInChargeRelayV2;
+	private boolean dataCreatedInAccountRelayV1;
+	private boolean dataCreatedInComboRelayV3;
+	private boolean dataCreatedInDeliveryWindowRelayV1;
+	private boolean dataCreatedInDealRelayV3;
+	private boolean dataCreatedInComboScoreRelayV3;
+	private boolean dataCreatedInPromotionRelayV3;
+
+	public boolean getDataCreatedInPriceRelayV2() {
+
+		return dataCreatedInPriceRelayV2;
+	}
+
+	public void setDataCreatedInPriceRelayV2(final boolean dataCreatedInPriceRelayV2) {
+
+		this.dataCreatedInPriceRelayV2 = dataCreatedInPriceRelayV2;
+	}
+
+	public boolean getDataCreatedInPriceRelayV3() {
+
+		return dataCreatedInPriceRelayV3;
+	}
+
+	public void setDataCreatedInPriceRelayV3(final boolean dataCreatedInPriceRelayV3) {
+
+		this.dataCreatedInPriceRelayV3 = dataCreatedInPriceRelayV3;
+	}
+
+	public boolean getDataCreatedInDealRelayV2() {
+
+		return dataCreatedInDealRelayV2;
+	}
+
+	public void setDataCreatedInDealRelayV2(final boolean dataCreatedInDealRelayV2) {
+
+		this.dataCreatedInDealRelayV2 = dataCreatedInDealRelayV2;
+	}
+
+	public boolean getDataCreatedInChargeRelayV2() {
+
+		return dataCreatedInChargeRelayV2;
+	}
+
+	public void setDataCreatedInChargeRelayV2(final boolean dataCreatedInChargeRelayV2) {
+
+		this.dataCreatedInChargeRelayV2 = dataCreatedInChargeRelayV2;
+	}
+
+	public boolean getDataCreatedInAccountRelayV1() {
+
+		return dataCreatedInAccountRelayV1;
+	}
+
+	public void setDataCreatedInAccountRelayV1(final boolean dataCreatedInAccountRelayV1) {
+
+		this.dataCreatedInAccountRelayV1 = dataCreatedInAccountRelayV1;
+	}
+
+	public boolean getDataCreatedInComboRelayV3() {
+
+		return dataCreatedInComboRelayV3;
+	}
+
+	public void setDataCreatedInComboRelayV3(final boolean dataCreatedInComboRelayV3) {
+
+		this.dataCreatedInComboRelayV3 = dataCreatedInComboRelayV3;
+	}
+
+	public boolean getDataCreatedInDeliveryWindowRelayV1() {
+
+		return dataCreatedInDeliveryWindowRelayV1;
+	}
+
+	public void setDataCreatedInDeliveryWindowRelayV1(final boolean dataCreatedInDeliveryWindowRelayV1) {
+
+		this.dataCreatedInDeliveryWindowRelayV1 = dataCreatedInDeliveryWindowRelayV1;
+	}
+
+	public boolean getDataCreatedInDealRelayV3() {
+
+		return dataCreatedInDealRelayV3;
+	}
+
+	public void setDataCreatedInDealRelayV3(final boolean dataCreatedInDealRelayV3) {
+
+		this.dataCreatedInDealRelayV3 = dataCreatedInDealRelayV3;
+	}
+
+	public boolean getDataCreatedInComboScoreRelayV3() {
+
+		return dataCreatedInComboScoreRelayV3;
+	}
+
+	public void setDataCreatedInComboScoreRelayV3(final boolean dataCreatedInComboScoreRelayV3) {
+
+		this.dataCreatedInComboScoreRelayV3 = dataCreatedInComboScoreRelayV3;
+	}
+
+	public void setDataCreatedInPromotionRelayV3(final boolean dataCreatedInPromotionRelayV3) {
+
+		this.dataCreatedInPromotionRelayV3 = dataCreatedInPromotionRelayV3;
+	}
+}
